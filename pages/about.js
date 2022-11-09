cc996aadb402fd0d220d47abe449958182fbd170
elek8871 <elekmichelle@gmail.com>
2022-11-09 14:59:21 -0700

    adds project cards

--- pages/about.js
+++ pages/about.js
@@ -6,42 +6,43 @@ import Link from "next/link"
 export default function About(){
     return(
         <div className="text-teal-700 font-serif">
-            <div>
-                <h2 className="text-teal-800 text-2xl font-semibold p-5 flex justify-center"> About Me </h2>
-                    <p className="pl-5 pt-2 flex justify-center"> I am forever in awe of what can be created with a few lines of code! I am passionate about learning and growing as an engineer while affecting positive change in the world around me. </p>
-                    <p className="pl-5 pt-2 flex justify-center"> I have strong communication and problem-solving skills, honed through my previous career in healthcare. I pride myself on my ability to connect with people of diverse backgrounds </p>
-            </div>
+                <div>
+                    <h2 className="text-teal-800 text-2xl font-semibold p-5 flex justify-center"> About Me </h2>
+                        <p className="pl-5 pt-2 flex justify-center"> I am forever in awe of what can be created with a few lines of code! I am passionate about learning and growing as an engineer while affecting positive change in the world around me. </p>
+                        <p className="pl-5 pt-2 flex justify-center"> I have strong communication and problem-solving skills, honed through my previous career in healthcare. I pride myself on my ability to connect with people of diverse backgrounds. </p>
+                </div>
         {/* // load img locallly from img folder */}
-            <div  className="p-4 flex justify-center">
-                <Image
-                    className="rounded-full"
-                    src ="/michelle.jpg"
-                    alt ="michelle in the library"
-                    width={400}
-                    height={300}
-                />
-            </div>
+                <div  className="p-4 flex justify-center">
+                    <Image
+                        className="rounded-full shadow-md"
+                        src ="/michelle.jpg"
+                        alt ="michelle in the library"
+                        width={400}
+                        height={300}
+                    />
+                </div>
 
-            <div>
-                <h2 className="text-teal-800 text-2xl font-bold p-4 flex justify-center"> Skills </h2>
-                <p className="pl-5 flex justify-center"> HTML * CSS * JavaScript * Node * React * Python * Express * Postgres * non-sql DBs * MongoDB * Django * Git * Git Hub </p>
-            </div>
+                <div>
+                    <h2 className="text-teal-800 text-2xl font-bold p-4 flex justify-center"> Skills </h2>
+                    <p className="pl-5 flex justify-center"> HTML * CSS * JavaScript * Node * React * Python * Express * Postgres * non-sql DBs * MongoDB * Django * Git * Git Hub </p>
+                </div>
+    
 
             <footer>
                 <div> 
                     <h1 className="text-teal-800 text-2xl font-semi-bold p-4 flex justify-center" > Contact Me </h1>
                     {/* need direct links w  icons */}
-                        <ul className="pl-5 flex justify-center space-x-8">
+                        <div className="pl-5 flex justify-center space-x-8">
                             <Link href="https://github.com/elek8871" target="_blank">
                                 <a>Git Hub</a>
                             </Link>
                             <Link href="https://www.linkedin.com/in/michelle-elek/" target="_blank"> 
                                 <a>LinkedIn </a>
                             </Link>
-                            {/* need to link to email and resume */}
-                            {/* li href="elekmichelle@gmail.com"> Email </li> */}
-                            {/* <li> Resume </li> */}
-                        </ul>
+                            <Link href="https://docs.google.com/document/d/1h5UoX7ne6breWCU2WWb3ml9eHRHDreoQ6mQNCiZ3aJM/edit?usp=sharing" target="_blank" >
+                                <a> View my Resume </a>
+                            </Link>
+                        </div>
                 </div>
             </footer>
     </div>
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -4,67 +4,90 @@ import Link from "next/link"
 export default function Portfolio(){
 
     return(
-        <div className="text-teal-700 font-serif flex justify-center">
-            <h1 className="text-2xl p-4 flex justify-center"> My Projects </h1>
+        <div className="text-teal-700 font-serif">
+            <h1 className="text-3xl p-3 flex justify-center"> My Projects </h1>
+            <p className="flex justify-center"> Click on the title of the project to view the live site</p>
+
+            {/* div that holds all cards */}
+            <div className="">
 
             {/* goal digger div */}
-            <div className="p-4">
-                <Link href="https://helpful-dusk-923e50.netlify.app/">
-                <a className="flex justify-center"> Goal Digger </a>
-                </Link>
-                    <Image
-                        className="rounded-md"
-                        src ="/goaldigger.png"
-                        alt ="home screeen of web site"
-                        width={400}
-                        height={280}
-                     />
-                    <ul className="font-light text-sm p-4">
-                        <li>Goal Digger is a full-stack, full CRUD, MERN application that allows users to create and edit their own bucket lists. </li>
-                        <li>Tech Stack: React.js, JS, MongoDB, Mongoose, Express, HTML, Vanilla CSS, React Bootstrap, external API </li>
-                    </ul>
-            </div>
+                <div className="p-4 m-5 bg-slate-300 rounded-md border-2 border-slate-500 ">
+                    <div className="flex justify-center text-2xl">
+                        <Link href="https://helpful-dusk-923e50.netlify.app/">
+                            <a> Goal Digger </a>
+                        </Link>
+                    </div>
+
+                    <div className="grid grid-flow-col auto-cols-auto">
+                        <Image
+                            className="rounded-md"
+                            src ="/goaldigger.png"
+                            alt ="home screeen of goal digger site"
+                            width={400}
+                            height={280}
+                            />
+                        <ul className="font-light text-med p-4">
+                            <li>Goal Digger is a full-stack, full CRUD, MERN application that allows users to create and edit their own bucket lists. </li>
+                            <li>Tech Stack: React.js, JS, MongoDB, Mongoose, Express, HTML, Vanilla CSS, React Bootstrap, external API </li>
+                        </ul>
+                    </div>
+                </div>
 
             {/* pups and pubs div */}
-            <div className="p-4">
-                <Link href="https://pubs-and-pubs.fly.dev/">
-                <a className="flex justify-center text-center"> Pups and Pubs </a>
-                </Link>
+                <div className="p-4 m-5 bg-slate-300 rounded-md border-2 border-slate-500">
+                    <div className="flex justify-center text-2xl">
+                        <Link href="https://pubs-and-pubs.fly.dev/">
+                        <a> Pups and Pubs </a>
+                        </Link>
+                    </div>
 
-                <Image
-                    className="rounded-md"
-                    src ="/pupsnpubs.png"
-                    alt ="home screeen of web site"
-                    width={400}
-                    height={280}
-                />
-                    <ul className="text-sm font-light p-4">
-                        <li>
-                            Pups and Pubs in a full-stack, full CRUD web application in which users can create an account, find and rate local breweries based on dog-friendliness and food and beverage quality.
-                        </li>
-                        <li>
-                            Tech Stack: Axios, SQL, Bootstrap CSS, HTML, JS, Express, Git, External Api
-                        </li>
-                    </ul>
-            </div>
+                    <div className="grid grid-flow-col auto-cols-auto">
+                        <div className="order-2">
+                        <Image
+                            className="rounded-md"
+                            src ="/pupsnpubs.png"
+                            alt ="home screeen of pups and pubs site"
+                            width={400}
+                            height={280}
+                        />
+                        </div>
+                        <div className="order-1">
+                            <ul className="text-sm font-light p-4">
+                                <li>
+                                    Pups and Pubs in a full-stack, full CRUD web application in which users can create an account, find and rate local breweries based on dog-friendliness and food and beverage quality.
+                                </li>
+                                <li>
+                                    Tech Stack: Axios, SQL, Bootstrap CSS, HTML, JS, Express, Git, External Api
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
 
             {/* memory match div */}
-            <div className="p-4">
-                    <Link href= "https://elek8871.github.io/SEI-Project-1---Match-Game/">
-                        <a className="flex justify-center text-center"> Memory Match Game </a>
-                    </Link>
+                <div className="p-4 m-5 bg-slate-300 rounded-md border-2 border-slate-500 ">
+                    <div div className="flex justify-center text-2xl">
+                        <Link href= "https://elek8871.github.io/SEI-Project-1---Match-Game/">
+                            <a className="flex justify-center text-center"> Memory Match Game </a>
+                        </Link>
+                    </div>
 
-                    <Image
-                        className="rounded-md"
-                        src ="/memorymatch.png"
-                        alt ="home screeen of web site"
-                        width={400}
-                        height={280}
-                    />
-                    <ul className="text-sm font-light p-4">
-                        <li> A fun retro hockey-themed memory match game, eloquently simple. </li>
-                        <li> Tech Stack : vanilla JS, HTML, CSS, and DOM manipulation</li>
-                    </ul>
+                    <div className = "grid grid-flow-col auto-cols-auto">
+                        <Image
+                            className="rounded-md"
+                            src ="/memorymatch.png"
+                            alt ="home screeen of memory match site"
+                            width={300}
+                            height={250}
+                        />
+                        <ul className="text-sm font-light p-4">
+                            <li> A fun retro hockey-themed memory match game, eloquently simple. </li>
+                            <li> Tech Stack : vanilla JS, HTML, CSS, and DOM manipulation</li>
+                        </ul>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
